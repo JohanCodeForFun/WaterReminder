43ce925a95c7fd80e51db83313c34c2ca4e677f9
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-10-21 22:17:41 +0200

    add fix to test for component import

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: "react-native",
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+  },
+  moduleFileExtensions: ["js", "jsx", "json", "node"]
 }
